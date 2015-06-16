5102b42d2f8430794996bf90ca45c1bd78c15d9a
TankyG <Grendagus@gmail.com>
2015-06-16 23:20:05 +0200

    erg
    Still does not work...

--- mainBot.py
+++ mainBot.py
@@ -2,49 +2,49 @@ import socket
 import string
 import time
 
-
 server = "efnet.port80.se"
 channel = "#123"
 botnick = "Kjell"
+password = ""
+
 
 def ping():
-    ircsock.send("Pong: ping\n")
+    i.send("Pong: ping\n")
 
 def sendmsg(recipient, msg):  # This is the send message function, it simply sends messages to the channel.
-    ircsock.send("PRIVMSG " + recipient + " :" + msg + "\n")
-
+    i.send("PRIVMSG " + recipient + " :" + msg + "\n")
 
 
 def joinchan(chan):  # This function is used to join channels.
-    ircsock.send("JOIN " + chan + "\n")
+    i.send("JOIN " + chan + "\n")
 
-ircsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-ircsock.connect((server, 6667)) # kobler til serveren som vi allerede har definert.
-ircsock.send("USER " + botnick + " " + botnick + " " + botnick + " Noe dritt \n")
-ircsock.send("NICK " + botnick + "\n")
+i = socket.socket()
+i.connect((server, 6667)) # kobler til serveren som vi allerede har definert.
+i.send("USER " + botnick + " " + botnick + " " + botnick + " Noe dritt \n")
+i.send("NICK " + botnick + "\n")
 joinchan(channel)
 
 
-x=0
+
 
 while 1:
-    ircmsg = ircsock.recv(1024)
+    ircmsg = i.recv(1024)
 
     print (ircmsg)
 
     if "PING :" in ircmsg:
         ping()
+        
 
-    if ":Test" in ircmsg and channel in ircmsg:
-        tst = commands.Test(words)
-        ircmsg(tst)
-        while x<10:
-            x+1
-            print " /n"
+    elif ":Test" in ircmsg and channel in ircmsg:
+        sendmsg(channel + "Test")
+
+
+
+        
+       
 
     
         
-  
-            
 
 
